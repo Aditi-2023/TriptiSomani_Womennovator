43ad366c96441d019a91a5242f90c997ac0cdbe4
sujata <sujatagahlaut2017@gmail.com>
2023-09-06 10:46:32 +0530

    pt. 5 done

--- journey.php
+++ journey.php
@@ -1,103 +1,136 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php 
+<?php
 include_once('header.php');
- include_once('conf/Db.php');
-   //$orderlimit=$driverObj->orderbylimit('tbl_gallery',0,6);
-   //print_r($orderlimit);
+include_once('conf/Db.php');
+//$orderlimit=$driverObj->orderbylimit('tbl_gallery',0,6);
+//print_r($orderlimit);
 ?>
-    <!--Navbar end-->
-    
-   <div class="section-parallax" style="background-image: url(img/images/4.jpg);background-position: bottom;">
-            <div class="parallax">
-                <div class="container my-5 pt-md-5">
-                    <div class="text-center pt-md-5 pt-4">
-                        <h1 class="display-4 text-white dm-font mb-2">Her Journey</h1> 
-                    </div>
-                </div>
+<!--Navbar end-->
+
+<div class="section-parallax" style="background-image: url(img/images/4.jpg);background-position: bottom;">
+    <div class="parallax">
+        <div class="container my-5 pt-md-5">
+            <div class="text-center pt-md-5 pt-4">
+                <h1 class="display-4 text-white dm-font mb-2">Her Journey</h1>
             </div>
         </div>
-    
-    <div class="" style="background: bisque;">  
+    </div>
+</div>
+
+<div class="" style="background: bisque;">
     <div class="container">
         <div class="timeline right row">
-            <div class="timeline-row"> 
+            <div class="timeline-row">
                 <div class="timeline-content">
                     <img src="img/j1.png" style="width: 100px;margin-bottom: 25px;">
                     <h5 class="mb-2">Grant Thornton</h5>
                     <p class="caption">I started my career as an intern with Grant Thornton
-in their Assurance and Not for Profit practice . I
-travelled the country reviewing the utilisation of
-funds of different NGOs. I worked with the World
-bank and uncovered a major fraud in a funded
-project for which I got letters of appreciation from the
-Integrity Vice Presidency Unit (INTVP).
-</p>
+                        in their Assurance and Not for Profit practice . I
+                        travelled the country reviewing the utilisation of
+                        funds of different NGOs. I worked with the World
+                        bank and uncovered a major fraud in a funded
+                        project for which I got letters of appreciation from the
+                        Integrity Vice Presidency Unit (INTVP).
+                    </p>
                 </div>
             </div>
-            <div class="timeline-row"> 
+            <div class="timeline-row">
                 <div class="timeline-content">
-                    <img src="img/j2.png" style="width: 100px;margin-bottom: 25px;"> 
+                    <img src="img/j2.png" style="width: 100px;margin-bottom: 25px;">
                     <h5 class="mb-2">PWC</h5>
                     <p class="caption">I joined the PwC Mergers & Acquisition
-division where in addition to private equity
-business clients in India, I also worked with
-the Japan desk. I was passionate about my job
-and quite good at it which is why I got 2
-promotions in 5 years wherein I joined as an
-Associate and left as a Manager.</p>
+                        division where in addition to private equity
+                        business clients in India, I also worked with
+                        the Japan desk. I was passionate about my job
+                        and quite good at it which is why I got 2
+                        promotions in 5 years wherein I joined as an
+                        Associate and left as a Manager.</p>
                 </div>
-            </div>   
+            </div>
+            <div class="timeline-row">
+                <div class="timeline-content">
+                    <img src="img/j1.png" style="width: 100px;margin-bottom: 25px;">
+                    <h5 class="mb-2">KGS Advisors</h5>
+                    <p class="caption">she heads KGS Advisors which is the financial advisory division of KGS. KGS
+                        Advisor work with various domestic and international startups and family offices. Our fintech
+                        arm creates data rooms and data analytics tools for banks, financial institutions, investors,
+                        and other key stakeholders.
+                    </p>
+                </div>
+            </div>
+            <div class="timeline-row">
+                <div class="timeline-content">
+                    <img src="img/j2.png" style="width: 100px;margin-bottom: 25px;">
+                    <h5 class="mb-2">Womennovator</h5>
+                    <p class="caption">she started womennovator as a global incubator to help nascent female startups
+                        and SMBs through resources, access to industry mentors, interactions with entrepreneurs, and
+                        capital with a network of 10,000 women entrepreneurs across 100 cities in India and 20 other
+                        countries. </p>
+                </div>
+            </div>
         </div>
-         <div class="col-md-10 mx-auto pb-5"> 
-             <div class="px-md-5 px-4 py-4 bg-white shadow rounded-3" style="">
-                    <div class="text-start">
-                        <h1 class="display-6 dm-font mb-4">Her journey as Chartered accountant </h1> 
+        <div class="col-md-10 mx-auto pb-5">
+            <div class="px-md-5 px-4 py-4 bg-white shadow rounded-3" style="">
+                <div class="text-start">
+                    <h1 class="display-6 dm-font mb-4">Her journey as Chartered accountant </h1>
+                </div>
+                <p class="mb-4">Chartered Accountant and post graduate in law with wide experience in business and
+                    financial advisory for various multi-national organizations such
+                    as PwC Grant Thornton (GT) World Bank (on behalf of GT) etc comprising due diligence reviews
+                    assisting clients in identifying postacquisition/integration issues forensic audits statutory &
+                    internal audits and other business advisory services. Experience includes various
+                    multinational foreign and domestic clients spread over a range of industries and been involved in a
+                    number of cross border transactions both
+                    outbound and inbound on behalf of strategic and financial investors. Tripti also does a lot of work
+                    for private equity clients in India and also coordinates for TS India’s efforts for private equity
+                    clients</p>
+                <p>On a functional basis, Tripti works across the deal continuum with a focus on due diligence. Her
+                    experience includes assisting in an assignment of
+                    fraud investigation with World Bank D.C officials. In addition to this Tripti’s experience includes
+                    two years of involvement in statutory audits internal
+                    audits and various other special assignments
+                </p>
+
+                <div class="py-4">
+                    <h4>Select credentials include:</h4>
+
+                    <div class="pt-2">
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">Ashoka Buildcon Limited</span>
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">JHS Svendgaard Laboratories
+                            Limited</span>
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">Carzonrent India Private Ltd.</span>
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">Kiri Dyes and Chemicals Ltd</span>
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">Isuzu Motors limited</span>
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">Anand Nishikawa Co</span>
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">Innodata Pvt Ltd</span>
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">Ranbaxy</span>
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">DLF services limited</span>
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">World Bank</span>
+                        <span class="badge rounded bg-secondary px-4 py-2 mb-1">Maruti Udyog Limited</span>
                     </div>
-                    <p class="mb-4">Chartered Accountant and post graduate in law with wide experience in business and financial advisory for various multi-national organizations such
-as PwC Grant Thornton (GT) World Bank (on behalf of GT) etc comprising due diligence reviews assisting clients in identifying postacquisition/integration issues forensic audits statutory & internal audits and other business advisory services. Experience includes various
-multinational foreign and domestic clients spread over a range of industries and been involved in a number of cross border transactions both
-outbound and inbound on behalf of strategic and financial investors. Tripti also does a lot of work for private equity clients in India and also coordinates for TS India’s efforts for private equity clients</p> 
-                    <p>On a functional basis, Tripti works across the deal continuum with a focus on due diligence. Her experience includes assisting in an assignment of
-fraud investigation with World Bank D.C officials. In addition to this Tripti’s experience includes two years of involvement in statutory audits internal
-audits and various other special assignments
-</p>
-                 
-                 <div class="py-4">
-                 <h4>Select credentials include:</h4>
-                     
-                     <div class="pt-2">
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">Ashoka Buildcon Limited</span>
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">JHS Svendgaard Laboratories Limited</span>
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">Carzonrent India Private Ltd.</span>
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">Kiri Dyes and Chemicals Ltd</span>
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">Isuzu Motors limited</span>
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">Anand Nishikawa Co</span>
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">Innodata Pvt Ltd</span>
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">Ranbaxy</span>
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">DLF services limited</span>
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">World Bank</span>
-                         <span class="badge rounded bg-secondary px-4 py-2 mb-1">Maruti Udyog Limited</span> 
-                     </div>
-                 </div>
                 </div>
-             
-             
-              <iframe width="100%" height="518" class="shadow my-5" src="https://www.youtube.com/embed/QIP3RN55slY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="margin-top: -0px;border-radius: 15px;"></iframe>
-             
-           
+            </div>
+
+
+            <iframe width="100%" height="518" class="shadow my-5" src="https://www.youtube.com/embed/QIP3RN55slY"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen style="margin-top: -0px;border-radius: 15px;"></iframe>
+
+
         </div>
-</div>
     </div>
+</div>
 
 
-    
 
 
-    <!-- footer section start -->
- <?php include_once('footer.php')?>
- 
+
+<!-- footer section start -->
+<?php include_once('footer.php') ?>
+
 
 
 </body>
